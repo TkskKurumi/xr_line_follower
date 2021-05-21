65d3fe70e7f9322da0fc5abeb25d780d8d501e0c
TkskKurumi <zafkielkurumi@gmail.com>
2021-05-21 10:00:58 +0800

    pos.msg float direction

--- src/linedetect.cpp
+++ src/linedetect.cpp
@@ -19,7 +19,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *@copyright Copyright 2017 Sudarshan Raghunathan
 *@file linedetect.cpp
 *@author Sudarshan Raghunathan
-*@brief  Class linedetect's function definitions
+*@brief    Class linedetect's function definitions
 */
 #include "linedetect.hpp"
 #include <cv_bridge/cv_bridge.h>
@@ -31,30 +31,35 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "ros/console.h"
 #include "xr_line_follower/pos.h"
 #include <sensor_msgs/CompressedImage.h>
-
-void LineDetect::imageCallback(const sensor_msgs::CompressedImageConstPtr &msg) {
+#define CV_FONT_HERSHEY_COMPLEX 3
+void LineDetect::imageCallback(const sensor_msgs::CompressedImageConstPtr &msg)
+{
   cv_bridge::CvImagePtr cv_ptr;
-  try {
+  try
+  {
     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     img = cv_ptr->image;
     cv::waitKey(30);
   }
-  catch (cv_bridge::Exception& e) {
+  catch (cv_bridge::Exception &e)
+  {
     // ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   }
 }
 
-cv::Mat LineDetect::Gauss(cv::Mat input) {
+cv::Mat LineDetect::Gauss(cv::Mat input)
+{
   cv::Mat output;
-// Applying Gaussian Filter
+  // Applying Gaussian Filter
   cv::GaussianBlur(input, output, cv::Size(3, 3), 0.1, 0.1);
   return output;
 }
 
-int LineDetect::colorthresh(cv::Mat input) {
+int LineDetect::colorthresh(cv::Mat input)
+{
   // Initializaing variables
   cv::Size s = input.size();
-  std::vector<std::vector<cv::Point> > v;
+  std::vector<std::vector<cv::Point>> v;
   auto w = s.width;
   auto h = s.height;
   auto c_x = 0.0;
@@ -65,62 +70,81 @@ int LineDetect::colorthresh(cv::Mat input) {
   //LineDetect::LowerYellow = {20, 100, 100};
   //LineDetect::UpperYellow = {30, 255, 255};
   cv::inRange(LineDetect::img_hsv, LowerYellow,
-   UpperYellow, LineDetect::img_mask);
-  img_mask(cv::Rect(0, 0, w, 0.8*h)) = 0;
+              UpperYellow, LineDetect::img_mask);
+  img_mask(cv::Rect(0, 0, w, 0.8 * h)) = 0;
   // Find contours for better visualization
   cv::findContours(LineDetect::img_mask, v, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
   // If contours exist add a bounding
   // Choosing contours with maximum area
-  if (v.size() != 0) {
-  auto area = 0;
-  auto idx = 0;
-  auto count = 0;
-  while (count < v.size()) {
-    if (area < v[count].size()) {
-       idx = count;
-       area = v[count].size();
+  if (v.size() != 0)
+  {
+    auto area = 0;
+    auto idx = 0;
+    auto count = 0;
+    while (count < v.size())
+    {
+      if (area < v[count].size())
+      {
+        idx = count;
+        area = v[count].size();
+      }
+      count++;
     }
-    count++;
-  }
-  cv::Rect rect = boundingRect(v[idx]);
-  cv::Point pt1, pt2, pt3;
-  pt1.x = rect.x;
-  pt1.y = rect.y;
-  pt2.x = rect.x + rect.width;
-  pt2.y = rect.y + rect.height;
-  pt3.x = pt1.x+5;
-  pt3.y = pt1.y-5;
-  // Drawing the rectangle using points obtained
-  rectangle(input, pt1, pt2, CV_RGB(255, 0, 0), 2);
-  // Inserting text box
-  cv::putText(input, "Line Detected", pt3,
-    CV_FONT_HERSHEY_COMPLEX, 1, CV_RGB(255, 0, 0));
+    cv::Rect rect = boundingRect(v[idx]);
+    cv::Point pt1, pt2, pt3;
+    pt1.x = rect.x;
+    pt1.y = rect.y;
+    pt2.x = rect.x + rect.width;
+    pt2.y = rect.y + rect.height;
+    pt3.x = pt1.x + 5;
+    pt3.y = pt1.y - 5;
+    // Drawing the rectangle using points obtained
+    rectangle(input, pt1, pt2, CV_RGB(255, 0, 0), 2);
+    // Inserting text box
+    cv::putText(input, "Line Detected", pt3,
+                CV_FONT_HERSHEY_COMPLEX, 1, CV_RGB(255, 0, 0));
   }
   // Mask image to limit the future turns affecting the output
-  img_mask(cv::Rect(0.7*w, 0, 0.3*w, h)) = 0;
-  img_mask(cv::Rect(0, 0, 0.3*w, h)) = 0;
+  img_mask(cv::Rect(0.7 * w, 0, 0.3 * w, h)) = 0;
+  img_mask(cv::Rect(0, 0, 0.3 * w, h)) = 0;
   // Perform centroid detection of line
   cv::Moments M = cv::moments(LineDetect::img_mask);
-  if (M.m00 > 0) {
-    cv::Point p1(M.m10/M.m00, M.m01/M.m00);
+  if (M.m00 > 0)
+  {
+    cv::Point p1(M.m10 / M.m00, M.m01 / M.m00);
     cv::circle(LineDetect::img_mask, p1, 5, cv::Scalar(155, 200, 0), -1);
   }
-  c_x = M.m10/M.m00;
+  c_x = M.m10 / M.m00;
+  cv::Point ptc;
+  ptc.x = c_x;
+  ptc.y = h / 2;
   // Tolerance to chooise directions
   auto tol = 15;
   auto count = cv::countNonZero(img_mask);
   // Turn left if centroid is to the left of the image center minus tolerance
   // Turn right if centroid is to the right of the image center plus tolerance
   // Go straight if centroid is near image center
-  if (c_x < w/2-tol) {
+  if (c_x < w / 2 - tol && count)
+  {
     LineDetect::dir = 0;
-  } else if (c_x > w/2+tol) {
+    cv::putText(input, "Left", pt3,
+                CV_FONT_HERSHEY_COMPLEX, 1, CV_RGB(255, 0, 0));
+  }
+  else if (c_x > w / 2 + tol && count)
+  {
+    cv::putText(input, "Right", pt3,
+                CV_FONT_HERSHEY_COMPLEX, 1, CV_RGB(255, 0, 0));
     LineDetect::dir = 2;
-  } else {
+  }
+  else if (count)
+  {
+    cv::putText(input, "Straight", pt3,
+                CV_FONT_HERSHEY_COMPLEX, 1, CV_RGB(255, 0, 0));
     LineDetect::dir = 1;
   }
-  // Search if no line detected
-  if (count == 0) {
+  else
+  {
+    // Search if no line detected
     LineDetect::dir = 3;
   }
   // Output images viewed by the turtlebot
